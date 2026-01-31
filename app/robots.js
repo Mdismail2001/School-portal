dfa85d6a4d4d38f73c7c080cde5bd92e4bb96fff
Mdismail2001 <mdismail85773@gmail.com>
2026-01-31 16:04:00 +0800

    sitemap and robots file added

---
+++ app/robots.js
@@ -0,0 +1,9 @@
+export default function robots() {
+  return {
+    rules: {
+      userAgent: "*",
+      allow: "/",
+    },
+    sitemap: "https://e-mosjid-blog-page-tc74-git-921070-md-ismails-projects-9e10c918.vercel.app/sitemap.xml",
+  };
+}
---
+++ app/sitemap.js
@@ -0,0 +1,26 @@
+export default function sitemap() {
+const baseUrl = "https://e-mosjid-blog-page-tc74-git-921070-md-ismails-projects-9e10c918.vercel.app";
+
+  return [
+    {
+      url: `${baseUrl}/`,
+      lastModified: new Date(),
+    },
+    {
+      url: `${baseUrl}/soalan-lazim`,
+      lastModified: new Date(),
+    },
+    {
+      url: `${baseUrl}/carian-masjid`,
+      lastModified: new Date(),
+    },
+    {
+      url: `${baseUrl}/info-emasjid`,
+      lastModified: new Date(),
+    },
+    {
+      url: `${baseUrl}/semakan-permohonan`,
+      lastModified: new Date(),
+    },
+  ];
+}
